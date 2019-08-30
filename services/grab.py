975d2ed7a4c40ca67ce651fac6e2282203ab14d4
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-08-30 14:29:28 +0300

    Update grab.py

--- services/grab.py
+++ services/grab.py
@@ -3,6 +3,8 @@ from service import Service
 
 class Grab(Service):
     def send_sms(self):
+        name = ''.join(random.choice(string.ascii_letters) for _ in range(9))
+        mail = ''.join(random.choice(string.ascii_letters) for _ in range(9)) + '@gmail.com'
         self.session.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register',
-                          data={'phoneNumber': self.formatted_phone, 'countryCode': self.country_code, 'name': 'Grab',
-                                'email': 'Grab'})
+                          data={'phoneNumber': self.formatted_phone, 'countryCode': self.country_code, 'name': name,
+                                'email': mail})
