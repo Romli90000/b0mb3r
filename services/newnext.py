4ffa5e911542a456f3fa66bcfc8c794a3d186710
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-08-30 14:34:56 +0300

    Update newnext.py

--- services/newnext.py
+++ services/newnext.py
@@ -4,8 +4,9 @@ from service import Service
 
 class NewNext(Service):
     def send_sms(self):
+        name = ''.join(random.choice(string.ascii_letters) for _ in range(9))
         self.session.post('https://newnext.ru/graphql', json={'operationName': 'registration', 'variables': {
-            'client': {'firstName': 'Иван', 'lastName': 'Иванов', 'phone': self.formatted_phone,
+            'client': {'firstName': name, 'lastName': name + er, 'phone': self.formatted_phone,
                        'typeKeys': ['Unemployed']}},
                                                               'query': 'mutation registration($client: ClientInput!) {'
                                                                        '\n  registration(client: $client) {'
